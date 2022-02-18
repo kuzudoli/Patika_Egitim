f2056d73b438b9e0fabe3c91c9be1d49a19e644d
Enes Çakır <enescakkr@windowslive.com>
2022-02-18 16:49:52 +0300

    Update CREATE-JOIN-ALTER.sql

--- SQL/CREATE-JOIN-ALTER.sql
+++ SQL/CREATE-JOIN-ALTER.sql
@@ -14,11 +14,27 @@ SELECT book.id AS "KITAP ID",
 	   author.email AS "ALICI MAIL" FROM book 
 JOIN author ON author.id = book.author_id
 
-ALTER TABLE book
-ALTER COLUMN page_number SET NOT NULL
+ALTER TABLE author
+ADD UNIQUE(email)
 
+select *from author
 
+INSERT INTO author (first_name,last_name,email,birthday)
+			VALUES ('Enes','Çakır','jhuckerby0@google.de','2007-10-15')
+--ERROR:  duplicate key value violates unique constraint "author_email_key"
 
+ALTER TABLE author
+ADD CHECK(birthday>'1970-01-01')
+--Tarih kısıtlaması 
 
+DELETE FROM book
+WHERE author_id IN (SELECT id FROM author WHERE birthday<'1970-01-01')
+--Author tablosında kısıtlamaya uymayan verileri silmek için önce book tablosundaki ilişkili veriler silinmesi
 
+DELETE FROM author
+WHERE birthday<'1970-01-01'
+--İlişkili veriler siliniyor
 
+INSERT INTO author (first_name,last_name,email,birthday)
+			VALUES ('Enes','Çakır','jhuckerby0@google.de','1969-10-15')
+--ERROR:  new row for relation "author" violates check constraint "author_birthday_check"
